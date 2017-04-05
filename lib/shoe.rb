5b9878e90a344a1b62a1a3fa53553917c7479fee
github username <github email address>
2017-04-05 15:23:30 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,17 @@
+require "pry"
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
   def initialize(brand)
+    @brand = brand
+    BRANDS << brand if !BRANDS.include?(brand)
+  end
+
+  BRANDS = []
+
+  def brand=(brand)
     @brand = brand
   end
 
